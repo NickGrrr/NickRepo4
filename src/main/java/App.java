a8d9d4a8b8b635cc95610c8155b78ba4aeed6ca4
Nick <xStatiXx@yandex.ru>
2023-03-22 21:12:07 +0300

    Задача решена

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,24 @@
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+
+        System.out.println(bean.equals(bean2));
+        System.out.println(catBean.equals(catBean2));
+
+
+        ((ConfigurableApplicationContext)applicationContext).close();
+
     }
 }
